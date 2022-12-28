650beb25ed77bec315fc4c1be71a9056b8db4168
Nils Bokermann <nils.bokermann@bermuda.de>
2022-12-28 13:40:22 +0100

    update security config

--- Demo-Start/backend/src/main/java/de/bermuda/hero/backend/BackendSecurityConfiguration.java
+++ Demo-Start/backend/src/main/java/de/bermuda/hero/backend/BackendSecurityConfiguration.java
@@ -2,6 +2,7 @@ package de.bermuda.hero.backend;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,7 @@ import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@Configuration
 public class BackendSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {

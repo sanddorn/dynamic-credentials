b395d0629539fdc2749c61cbe4815ec3569938c2
Nils Bokermann <nils.bokermann@bermuda.de>
2023-04-10 18:43:40 +0200

    Config for Cypress

--- Demo-Authentication/IntegrationTest/cypress.config.js
+++ Demo-Authentication/IntegrationTest/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "onz467",
   e2e: {
     baseUrl: "http://localhost:8080",
     setupNodeEvents(on, config) {
